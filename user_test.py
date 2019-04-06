08313357bfbeca37f4e55d021c0f630c08b887ac
johnmwangi <jonesmwas356@gmail.com>
2019-04-06 13:45:44 +0300

    pass word run added

--- user_test.py
+++ user_test.py
@@ -24,12 +24,16 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,'Mwangi')
         self.assertEqual(self.new_user.password,'locker')
 
-    def test_save_user(self):
-        '''
-        Test to check if New user information is saved into the users_list
-        '''
-        self.new_user.save_user()
-        self.assertEqual(len(User.users_list),1)
-
 if __name__ == '__main__':
     unittest.main()
+
+
+#     def test_save_user(self):
+#         '''
+#         Test to check if New user information is saved into the users_list
+#         '''
+#         self.new_user.save_user()
+#         self.assertEqual(len(User.users_list),1)
+#
+# if __name__ == '__main__':
+#     unittest.main()
